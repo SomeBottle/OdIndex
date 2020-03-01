e7844c4c9518864ebe32ab5ffbff844bba5c3b11
SomeBottle <583797659@qq.com>
2020-03-02 01:09:41 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,247 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+$config=array(
+    "refresh_token"=>"",
+    "client_id"=>"",
+    "client_secret"=> "",
+    "redirect_uri"=> "https://heymind.github.io/tools/microsoft-graph-api-auth", 
+    'base'=>"/",
+	'sitepath'=>'',
+    "cache"=>array(
+        'enable'=>true
+    ),
+    'thumbnail'=>true,
+    'useProxy'=>true
+);
+function valueinarr($v,$a){/*判断数组中是否有一个值*/
+	foreach($a as $val){
+		if(stripos($val,strval($v))!==false){
+			return true;
+		}
+	}
+	return false;
+}
+function modifyarr($arr,$search,$v){/*搜素并编辑数组*/
+	foreach($arr as $k=>$val){
+		if(stripos($val,strval($search))!==false){
+			$arr[$k]=$v;
+			return $arr;
+		}
+	}
+}
+function request($u,$q,$method='POST',$head=array('Content-type:application/x-www-form-urlencoded'),$headerget=false,$retry=false) {/*poster*/
+    $rqcontent='';
+    if($method=='POST'){
+       $rqcontent=http_build_query($q);
+	}
+    $opts = array(
+        'http' => array(
+            'method' => $method,
+            'header' => $head,
+            'timeout' => 15 * 60, // 超时时间（单位:s）
+			'content'=>$rqcontent
+        )
+    );
+	if($headerget){
+	    stream_context_set_default($opts);
+		$hd=@get_headers($u,1);
+		if(valueinarr(401,$hd)&&!$retry){/*accesstoken需要刷新*/
+			$newtoken=getAccessToken(true);
+			$head=modifyarr($head,'Authorization','Authorization: bearer '.$newtoken);/*获得新token重新请求*/
+		    return request($u,$q,$method,$head,$headerget,true);
+		}
+		return $hd;
+	}else{
+        $ct = stream_context_create($opts);
+        $result = @file_get_contents($u, false, $ct);
+		$backhead=$http_response_header;/*获得返回头*/
+		if(valueinarr(401,$backhead)&&!$retry){/*accesstoken需要刷新*/
+			$newtoken=getAccessToken(true);
+			$head=modifyarr($head,'Authorization','Authorization: bearer '.$newtoken);/*获得新token重新请求*/
+		    return request($u,$q,$method,$head,$headerget,true);
+		}
+	    if(!empty($result)){
+            return $result;
+	    }else{
+		    return '';
+	    }
+	}
+}
+function parsepath($u){/*得出处理后的路径*/
+	global $config;
+	$up=parse_url($u)['path'];
+	return str_ireplace($config['sitepath'],'',$up);
+}
+function getAccessToken($update=false){/*获得AccessToken*/
+	global $config;
+	if($update||!file_exists('./token.php')){
+	$resp=request('https://login.microsoftonline.com/common/oauth2/v2.0/token',array(
+	    'client_id'=>$config['client_id'],
+		'redirect_uri'=>$config['redirect_uri'],
+		'client_secret'=>$config['client_secret'],
+		'refresh_token'=>$config['refresh_token'],
+		'grant_type'=>'refresh_token'
+	));
+	$data=json_decode($resp,true);
+	if(isset($data['access_token'])){
+		file_put_contents('./token.php','<?php $token="'.$data['access_token'].'";?>');
+		return $data['access_token'];
+	}else{
+		die('Failed to get accesstoken.');
+	}
+	}else{
+		require './token.php';
+		return $token;
+	}
+}
+function getParam($url,$param){/*获得url中的参数*/
+	$prs=parse_url($url)['query'];
+	$prs=explode('&',$prs);
+	$arr=array();
+	foreach($prs as $v){
+		$each=explode('=',$v);
+		if(isset($each[1])){
+		    $arr[$each[0]]=$each[1];
+		}else{
+			$arr[$each[0]]='';
+		}
+	}
+	if(isset($arr[$param])){
+	    return $arr[$param];
+	}else{
+		return '';
+	}
+}
+function handleRequest($url){
+	global $config;
+	$thumbnail=false;
+	$error='';
+	$accessToken=getAccessToken();/*获得accesstoken*/
+	$path=parsepath($url);
+	if($path=='/'){/*特殊处理*/
+		$path='';
+	}
+	if($config['thumbnail']){
+		$thumb=getParam($url,'thumbnail');/*获得缩略图尺寸*/
+		if(!empty($thumb)){
+			$thumbnail=$thumb;
+		}
+	}
+	if($thumbnail){/*如果是请求缩略图*/
+		$rq='https://graph.microsoft.com/v1.0/me/drive/root:'.$config['base'].$path.':/thumbnails/0/'.$thumbnail.'/content';
+		$resp=request($rq,'','GET',array(
+		   'Content-type: application/x-www-form-urlencoded',
+		   'Authorization: bearer '.$accessToken
+		),true);
+		if($resp){
+		return handleFile($resp['Location']);
+		}
+	}
+	/*Normally request*/
+	$path=urlencode($path);
+	$path=str_ireplace('+','%20',$path);
+	$rq='https://graph.microsoft.com/v1.0/me/drive/root:'.$config['base'].$path.'?select=name,eTag,size,id,folder,file,%40microsoft.graph.downloadUrl&expand=children(select%3Dname,eTag,size,id,folder,file)';
+	$resp=request($rq,'','GET',array(
+	    'Content-type: application/x-www-form-urlencoded',
+		'Authorization: bearer '.$accessToken
+    ));
+	if($resp){
+	$data=json_decode($resp,true);
+	if(isset($data['file'])){/*返回的是文件*/
+		handleFile($data["@microsoft.graph.downloadUrl"]);
+	}else if(isset($data['folder'])){/*返回的是目录*/
+		$render=renderFolderIndex($data['children'],parsepath($url));
+		return $render;
+	}else{
+		return 'Error response:'.var_export($resp,true);
+	}
+	}else{
+		return '<!--NotFound:'.$path.'-->';
+	}
+}
+function el($tag,$attrs,$content){
+	$attrs=join(' ',$attrs);
+	return '<'.$tag.' '.$attrs.'>'.$content.'</'.$tag.'>';
+}
+function div($className,$content){
+	return el('div',array('class='.$className),$content);
+}
+function item($icon,$filename,$size=false,$href=false){
+	if(!$href){
+		$href=$filename;
+	}
+	if($size){
+		$sizeitem='size="'.$size.'"';
+	}else{
+		$sizeitem='';
+	}
+	return el('a',array('href="'.$href.'"','class="item"',$sizeitem),el('i',array('class="material-icons"'),$icon).$filename);
+}
+function mime2icon($t){
+	if(stripos($t,'image')!==false){
+		return 'image';
+	}else if(stripos($t,'audio')!==false){
+		return 'audiotrack';
+	}else if(stripos($t,'video')!==false){
+		return 'video_label';
+	}else{
+		return 'description';
+	}
+}
+function renderFolderIndex($items,$isIndex){/*渲染目录列表*/
+	$nav='<nav><a class="brand">Bottle Od</a></nav>';
+	$itemrender='';
+	if($isIndex!=='/'){
+		$itemrender=item("folder", "..");/*在根目录*/
+	}
+	foreach($items as $v){
+		if(isset($v['folder'])){/*是目录*/
+			$itemrender.=item("folder", $v['name'], $v['size'],$v['name'].'/');
+		} else if (isset($v['file'])) {/*是文件*/
+			$itemrender.=item(mime2icon($v['file']['mimeType']), $v['name'], $v['size'],'./'.$v['name']);
+		}
+	}
+	return renderHTML($nav.div('container',div('items',el('div',array('style="min-width:600px"'),$itemrender))));
+}
+function renderHTML($body){
+	return '<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+      <title>Bottle Od</title>
+      <link href=\'https://fonts.loli.net/icon?family=Material+Icons\' rel=\'stylesheet\'>
+      <link href=\'https://cdn.jsdelivr.net/gh/heymind/OneDrive-Index-Cloudflare-Worker/themes/material.css\' rel=\'stylesheet\'>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/themes/prism-solarizedlight.css">
+      <script type="module" src=\'https://cdn.jsdelivr.net/gh/heymind/OneDrive-Index-Cloudflare-Worker/script.js\'></script>
+    </head>
+    <body>
+      '.$body.'
+      <div style="flex-grow:1"></div>
+      <footer><p>Original design by <a href="https://github.com/heymind/OneDrive-Index-Cloudflare-Worker">Heymind</a>.</p></footer>
+      <script src="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/prism.min.js" data-manual></script>
+      <script src="https://cdn.jsdelivr.net/npm/prismjs@1.17.1/plugins/autoloader/prism-autoloader.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/gh/SomeBottle/othumb.js@latest/othumb.m.js"></script>
+    </body>
+  </html>';
+}
+function handleFile($url){
+	global $config;
+	if($config['useProxy']){
+		$url=$config['sitepath'].'/odproxy.php?'.substr($url,6);
+		header('Location: '.$url);
+	}else{
+	    header('Location: '.substr($url,6));
+	}
+}
+/*Request Processor*/
+$pr=$_GET['q'];
+if(empty($pr)){
+	$pr='';
+}
+$self=substr($_SERVER['PHP_SELF'],strrpos($_SERVER['PHP_SELF'],'/')+1);
+$pr=str_ireplace($self,'',$pr);
+$requesturl='http://request.yes/'.$pr;
+echo handleRequest($requesturl);
+?>
---
+++ odproxy.php
@@ -0,0 +1,102 @@
+<?php
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+set_time_limit(0);
+$allowtags = array('1drv', 'onedrive', 'sharepoint', 'bottle.moe');
+$streamtype = array('mp4', 'mp3', 'm4a');
+$rq = explode('?', $_SERVER["REQUEST_URI"]) [1];
+$parsed = parse_url($rq);
+if (!isset($parsed['scheme'])) {
+    $rq = 'https:' . $rq;
+}
+function typetrans($arr) {
+    if (is_array($arr)) {
+        $t = '';
+        foreach ($arr as $v) {
+            $t.= $v . ';';
+        }
+        return $t;
+    } else {
+        return $arr;
+    }
+}
+function ex($arr, $v) { /*判断是否在数组里有匹配的*/
+    $rt = false;
+    if (!is_array($v)) {
+        foreach ($arr as $val) {
+            if (stripos($v, $val) !== false) {
+                $rt = true;
+            }
+        }
+    } else {
+        foreach ($arr as $val) {
+            foreach ($v as $val2) {
+                if (stripos($val2, $val) !== false) {
+                    $rt = $val2;
+                }
+            }
+        }
+    }
+    return $rt;
+}
+if (!empty($rq) && ex($allowtags, $rq)) {
+    $st = fopen($rq, 'r');
+    $hd = get_headers($rq, 1);
+    $bytes = $hd['Content-Length'];
+    $type = $hd['Content-Type'];
+    if ($bytes > 0) {
+        $dispostion = $hd['Content-Disposition']; /*获取文件信息*/
+        /*转发下载*/
+        header('Content-Description: File Transfer');
+        if (!$type) {
+            $type = 'application/octet-stream';
+        }
+        if (ex($streamtype, typetrans($type))) { /*流媒体*/
+            /*$contenttype = ex($streamtype, $type);
+            header("Content-type: " . $contenttype);
+            header("Accept-Ranges: bytes");
+            if (isset($_SERVER['HTTP_RANGE'])) {
+                header("HTTP/1.1 206 Partial Content");
+                list($name, $range) = explode("=", $_SERVER['HTTP_RANGE']);
+                list($begin, $end) = explode("-", $range);
+                if ($end == 0) {
+                    $end = $bytes - 1;
+                }
+            } else {
+                $begin = 0;
+                $end = $bytes - 1;
+            }
+            header("Content-Length: " . ($end - $begin + 1));
+            header("Content-Disposition: " . $dispostion);
+            header("Content-Range: bytes " . $begin . "-" . $end . "/" . $bytes);
+            fseek($st, $begin);
+            while (!feof($fp)) {
+                $p = min(1048576, $end - $begin + 1);
+                $begin+= $p;
+                echo fread($st, $p);
+            }*/
+        }
+            header('Content-Type: ' . $type);
+            header('Content-Disposition: ' . $dispostion);
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . $bytes);
+            @ob_start();
+            while (!feof($st)) {
+                $output = fread($st, 1048576);
+                echo $output;
+                ob_flush();
+                echo ob_get_clean();
+                flush();
+            }
+            fclose($st);
+            @ob_end_flush();
+        fclose($st);
+    } else {
+        echo 'Bad request:1';
+    }
+} else {
+    echo 'Bad request:2';
+}
+?>
